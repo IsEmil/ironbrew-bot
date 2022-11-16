66d77ed613e597a726e036a10c7a0a73676d3213
LocalSmail <96918131+LocalSmail@users.noreply.github.com>
2022-11-16 09:46:06 +0000

    Update ib2.js

--- ib2.js
+++ ib2.js
@@ -8,14 +8,18 @@ const fs = require("fs");
  */
 module.exports = (src) => {
     return new Promise((resolve, reject) => {
+       // Input = Inputted file by user
         const input = tmp.fileSync();
         const output = tmp.fileSync();
+        // Output = Obfuscated file returned by IronBrew
 
         fs.writeFileSync(input.name, src);
+        
+        // Spawns a process for IronBrew Client dll and parces the file name and output name
 
         const process = cp.spawn("dotnet", [path.join(__dirname, "/ib2/Source/IronBrew2 CLI.dll"), input.name, output.name], {
-            cwd: path.join(__dirname, "/ib2/Source"),
-            detached: true,
+            cwd: path.join(__dirname, "/ib2/Source"), // Joins the Current Working dir of the js file with the source folder
+            detached: true, // detaches
         });
 
         process.stderr.on("data", (data) => {
